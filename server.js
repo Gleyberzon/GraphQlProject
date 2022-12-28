3e0ee493a24e4c80cda3c018b54461147ebdc84e
shlomo mhadker <shlomo22m@gmail.com>
2022-12-28 13:55:24 +0200

    graphql v1

---
+++ server.js
@@ -0,0 +1,123 @@
+const express =require('express');
+const { buildSchema } = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
+const axios = require(`axios`);
+
+const app = express();
+
+
+/*
+ID
+String
+Int
+Float
+List[]
+Boolean
+*/
+
+let message = "this is a message";
+
+const schema = buildSchema(`
+
+        type Post{
+            userId: Int
+            id: Int
+            title: String
+            body: String
+        }
+
+        type User{
+            name: String
+            age: Int
+            college: String
+        }
+
+        type Query {
+            hello: String!
+            welcomeMessage(name:String, dayOfweek: String!): String
+            getUser:User
+            getUsers: [User]
+            getPostsFromExternalAPI: [Post]
+            message: String
+        }
+
+        input UserInput{
+            name: String!
+            age: Int!
+            college: String!
+        }
+
+        type Mutation{
+            setMesseage(newMessage: String): String
+            createUser(user: UserInput): User
+        }
+   `);
+
+   //createUser(name: String!, age: Int!, college: String!): User
+
+   const user = {
+    
+   }
+
+const root={
+    hello: ()=>{
+       return "hello world";
+       //return null; 
+    },
+    welcomeMessage: (args) => {
+        console.log(args);
+        return `hey ${args.name} today is ${args.dayOfweek}`;
+    },
+    getUser: ()=>{
+        const user = {
+            name: 'shlomo m',
+            age:34,
+            college: 'tec',
+        };
+        return user;
+    },
+    getUsers:()=>{
+    const users = [
+        {
+            name: 'shlomo m',
+            age:34,
+            college: 'tec',
+        },
+        {
+            name: 'shlomo mh',
+            age:340,
+            college: 'tecn',
+        }
+    ];
+    return users;
+},
+    getPostsFromExternalAPI: () => {
+      const result=await 
+      axios.get('https://jsonplaceholder.typicode.com/posts');
+      return result.data
+       /* return axios
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .then(result => result.data);*/
+        
+},
+setMesseage: ({newMessage}) => {
+    message=newMessage;
+    return message;
+},
+message: () => message,
+createUser: (args)=> {
+    console.log(args);
+    return args.user;
+}
+};
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+    graphiql: true,
+    schema: schema,
+    rootValue: root,
+}) 
+);
+
+app.listen(4000, () => console.log('server on port 4000'));
